fdf2275f6cc96f85bba92e10b7e08b50dc1cc2cf
rishabhraikwar98 <rishabhraikwar77@gmail.com>
2025-04-29 14:56:14 +0530

    added auth middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+const authenticate = async (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if (!token)
+    return res.status(401).json({ message: "Not authorized, no token" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select("-password");
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Not authorized, token failed" });
+  }
+};
+
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "User role not authorized" });
+    }
+    next();
+  };
+};
+module.exports = { authenticate, authorizeRoles };
