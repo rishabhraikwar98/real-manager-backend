3719584ad1f33fc80be50141ea5745e17f65a634
rishabhraikwar98 <rishabhraikwar77@gmail.com>
2025-05-13 11:28:23 +0530

    added project routes

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -9,16 +9,15 @@ const {
 } = require("../controllers/projectController");
 
 const {
-  authenticate,
   authorizeRoles,
 } = require("../middleware/authMiddleware");
 
 const router = express.Router();
-router.get("/engaged", authenticate, getEngagedProjects);
-router.get("/", authenticate, getAllProjects);
-router.get("/:id", authenticate, getProjectById);
-router.post("/", authenticate, authorizeRoles("admin"), createProject);
-router.patch("/:id",authenticate,authorizeRoles("admin", "manager"),updateProject);
-router.delete("/:id", authenticate, authorizeRoles("admin"), deleteProject);
+router.get("/engaged", getEngagedProjects);
+router.get("/", getAllProjects);
+router.get("/:id", getProjectById);
+router.post("/", authorizeRoles("admin"), createProject);
+router.patch("/:id", authorizeRoles("admin", "manager"), updateProject);
+router.delete("/:id", authorizeRoles("admin"), deleteProject);
 
 module.exports = router;
