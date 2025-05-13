2699b5b042f0a20c3eff9ce1a3f5e2b1fc6d792e
rishabhraikwar98 <rishabhraikwar77@gmail.com>
2025-05-13 11:26:45 +0530

    minor changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,11 +5,10 @@ const register = async (req, res) => {
   const { name, email, password, role } = req.body;
   try {
     const existingUser = await User.findOne({ email });
-    if (existingUser)
+    if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
-
+    }
     const user = await User.create({ name, email, password, role });
-
     const token = jwt.sign(
       { id: user._id, role: user.role },
       process.env.JWT_SECRET,
@@ -32,10 +31,14 @@ const login = async (req, res) => {
 
   try {
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+    if (!user) {
+      return res.status(400).json({ message: "You must signup first" });
+    }
+
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch)
+    if (!isMatch) {
       return res.status(400).json({ message: "Invalid credentials" });
+    }
 
     const token = jwt.sign(
       { id: user._id, role: user.role },
@@ -49,7 +52,7 @@ const login = async (req, res) => {
       maxAge: 90 * 24 * 60 * 60 * 1000, // 90 days in milliseconds
     });
 
-    res.status(200).json({ token, user });
+    res.status(200).json({ token,user});
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
